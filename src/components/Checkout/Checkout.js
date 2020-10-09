2ff44d396db3a5ab154b27f0558813617c54ee16
Juan Pablo Solano <juanpablosolanosuarez@hotmail.com>
2020-10-09 12:48:58 +0200

    Generated two routes (checkout and /)

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,21 +1,59 @@
 import React from "react";
 import Burger from "../Burger/Burger";
 import { connect } from "react-redux";
+import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router-dom";
+import * as actionTypes from "../../store/actions";
 import "./Checkout.css";
 
 const Checkout = (props) => {
+  let history = useHistory();
+  const cancelOrder = () => {
+    history.push("/");
+  };
+
+  const continueOrder = () => {
+    console.log("Hello");
+    history.push("/checkout/contact-data");
+  };
+  const { ingredients, onCloseModal, purchased } = props;
   return (
     <div>
       <h1 className="checkout-title">Just get this amazing Burger!</h1>
-      <Burger ingredients={props.ingredients} />
+      <Burger ingredients={ingredients} />
+      <div className="checkout-buttons">
+        <Button
+          className="cancel-button"
+          onClick={() => {
+            cancelOrder();
+            onCloseModal(!purchased);
+          }}
+        >
+          Cancel
+        </Button>
+        <Button className="continue-button" onClick={() => continueOrder()}>
+          Continue
+        </Button>
+      </div>
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
   return {
-    ingredients: state.ingredients
+    ingredients: state.ingredients,
+    purchased: state.purchased
   };
 };
 
-export default connect(mapStateToProps)(Checkout);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onCloseModal: (status) =>
+      dispatch({
+        type: actionTypes.PURCHASED_STATUS,
+        purchase: status
+      })
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
